c7c31e271f15c2e26263a825ef7fb5354a17cd37
Aricelio Fernandes <ariceliodesouza@gmail.com>
2022-07-23 18:11:36 -0300

    Criado as classes de repositório para os outros modelos.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    {
+
+    }
+    
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+
+        }
+    }
+}
