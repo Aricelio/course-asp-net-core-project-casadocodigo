af66acc4675f069de983955158f65a557a02bdab
Aricelio Fernandes <ariceliodesouza@gmail.com>
2022-08-16 20:41:20 -0300

    feat: alterado a view de carrinho para mostrar corretamente os produtos adicionados

--- Aulas/Aula1/CasaDoCodigo/Models/modelo.cs
+++ Aulas/Aula1/CasaDoCodigo/Models/modelo.cs
@@ -26,13 +26,13 @@ namespace CasaDoCodigo.Models
         [Required]
         public string Nome { get; private set; }
         [Required]
-        public decimal Preco { get; private set; }
+        public decimal Preco { get; private set; }        
 
-        public Produto(string codigo, string nome, decimal preco)
+        public Produto(string codigo, string nome, decimal preco, int quantidade)
         {
             this.Codigo = codigo;
             this.Nome = nome;
-            this.Preco = preco;
+            this.Preco = preco;            
         }
     }
 
