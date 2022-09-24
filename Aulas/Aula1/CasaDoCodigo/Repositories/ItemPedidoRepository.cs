390678fb585a3c1c304944eec27a0e575003e5f6
Aricelio de Souza Fernandes <ariceliosouza@hotmail.com>
2022-09-24 18:00:07 -0300

    fix: adicionado o método de remoção do item do pedido quando sua quantidade for zero

--- Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -9,13 +9,14 @@ namespace CasaDoCodigo.Repositories
     public interface IItemPedidoRepository
     {
         ItemPedido GetItemPedido(int itemPedidoId);
+        void RemoveItemPedido(int itemPedidoId);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
-
+            
         }
 
         public ItemPedido GetItemPedido(int itemPedidoId)
@@ -23,6 +24,11 @@ namespace CasaDoCodigo.Repositories
             return dbSet
                 .Where(ip => ip.Id == itemPedidoId)
                 .SingleOrDefault();
+        }
+
+        public void RemoveItemPedido(int itemPedidoId)
+        {
+            dbSet.Remove(GetItemPedido(itemPedidoId));
         }
     }
 }
