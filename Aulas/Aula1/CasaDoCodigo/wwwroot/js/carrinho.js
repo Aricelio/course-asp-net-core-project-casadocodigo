fb1af73f3f149f9db96cc335de86a6b1dfa4d424
Aricelio de Souza Fernandes <ariceliosouza@hotmail.com>
2022-09-26 20:50:54 -0300

    refactor: adicionado método de validação de token na função UpdateQuantidade

--- Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -29,11 +29,18 @@ class Carrinho {
     }
 
     postQuantidade(data) {
+
+        let token = $('[name=__RequestVerificationToken]').val();
+
+        let headers = {};
+        headers['RequestVerificationToken'] = token;
+
         $.ajax({
             url: '/pedido/updatequantidade',
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify(data)
+            data: JSON.stringify(data),
+            headers: headers
         }).done(function (response) {
             //location.reload();
             let itemPedido = response.itemPedido;
